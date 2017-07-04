68f64b475d41d5977ff52a0477ef15ea7d33b247
WolfBungalow <irtwit@gmail.com>
2017-07-03 20:04:21 -0700

    Update cull_snapshots.py
    Added more comments. Replaced some check_output with calls. Fixed indenting.

--- cull_snapshots.py
+++ cull_snapshots.py
@@ -14,9 +14,21 @@ def send_get(api_url):
 	return requests.get(config.serverURL + api_url, auth=config.auth,
 		headers={'Content-Type': 'application/json'})
 
-# Returns a list of all periodic snapshot tasks.
+# Call FreeNAS API to get the system's periodic snapshot tasks.
 def get_snapshottasks():
     return json.loads(send_get('/api/v1.0/storage/task/').text)
+
+# Query the API, create a list with the periodic task's filesystem and snapshot
+# combined retention count and unit. Example: '2' and 'week' becomes '2w'.
+def getTaskDetail():
+    snapTaskDetail = []
+    for a in get_snapshottasks():
+        b = [a['task_filesystem'], str(a['task_ret_count']) 
+            + a['task_ret_unit'][0:1]]
+        # Tasks with duplicate filesystem and retention critera are combined.
+        if b not in snapTaskDetail:
+            snapTaskDetail.append(b)
+    return snapTaskDetail
 
 # Loop through snapshots from old to new. Compare two at a time, deleting newer
 # snapshots with no difference from the older one.
@@ -24,13 +36,15 @@ def cull_IdenticalSnaps(snapTaskDetail):
 
     # Create system snap list. Get all system snapshots, their name and this
     # script's custom property. Split into list and sort from old to new.
-    args = ['zfs', 'list', '-H', '-t', 'snapshot', '-o', 'name,ast:culled']
-    sysSnaps = sorted(subprocess.check_output(args).decode('UTF-8').splitlines())
-
+    cmd = ['zfs', 'list', '-H', '-t', 'snapshot', '-o', 'name,ast:culled']
+    sysSnaps = sorted(subprocess.check_output(cmd, universal_newlines=True)
+                      .splitlines())
+                      
     for task_fileSystem, task_retention in snapTaskDetail:
         # RegEx system snaps list, match task filesystem and retention critera.
         pattern = '^' + task_fileSystem + '@auto-.*' + task_retention
-        taskSnaps = [line.split("\t") for line in sysSnaps if re.search(pattern, line)]
+        taskSnaps = [line.split("\t") for line in sysSnaps
+                     if re.search(pattern, line)]
 
         # Return if not enough snaps to compare.
         if len(taskSnaps) < 2: return
@@ -42,9 +56,9 @@ def cull_IdenticalSnaps(snapTaskDetail):
             # found set custom property, else no diff found, destroy the snap.
             if culled != 'True':
                 if subprocess.check_output(['zfs', 'diff', lastSnap, snapName]):
-                    subprocess.check_output(['zfs', 'set', 'ast:culled=True', snapName])
+                    subprocess.call(['zfs', 'set', 'ast:culled=True', snapName])
                 else:
-                    subprocess.check_output(['zfs', 'destroy', snapName])
+                    subprocess.call(['zfs', 'destroy', snapName])
                     continue
             lastSnap = snapName
 
@@ -52,9 +66,10 @@ def cull_SnapsByDate(snapTaskDetail):
 
     # Create system snap list. Get all system snapshots names. Split into list
     # and sort from new to old.
-    args = ['zfs', 'list', '-H', '-t', 'snapshot', '-o', 'name']
-    sysSnaps = sorted(subprocess.check_output(args).decode('UTF-8').splitlines(), reverse=True)
-    
+    cmd = ['zfs', 'list', '-H', '-t', 'snapshot', '-o', 'name']
+    sysSnaps = sorted(subprocess.check_output(cmd, universal_newlines=True)
+                      .splitlines(), reverse=True)
+
     today = datetime.datetime.today()
 
     for task_fileSystem, task_retention in snapTaskDetail:
@@ -65,16 +80,18 @@ def cull_SnapsByDate(snapTaskDetail):
         lastCull = ''
         for snapName in taskSnaps:
             # Create a datetime from the date in the snap name.
-            snapDate = datetime.datetime.strptime(snapName.split('@')[1][5:18], '%Y%m%d.%H%M')
+            snapDate = datetime.datetime.strptime(
+                snapName.split('@')[1][5:18], '%Y%m%d.%H%M')
             snapDaysOld = (today - snapDate).days
 
             # Keep all snaps less than X days old.
             if snapDaysOld < config.snapAgeMin: continue
 
-            # Get the date string used to decide whether to keep a snap. If older
+            # Get the date string for deciding whether to keep a snap. If older
             # than X days return %Year%YearWeek; only the newest snap per week
             # will be saved. Else only the newest snap per day will be saved.
-            cullDateStr = snapDate.strftime('%Y%U' if snapDaysOld > config.snapAgeDay else '%Y%m%d')
+            cullDateStr = snapDate.strftime(
+                '%Y%U' if snapDaysOld > config.snapAgeDay else '%Y%m%d')
 
             # Starting from the newest snap, skip if cull date string doesn't
             # match the last one; either it's the first loop or a different day
@@ -82,20 +99,18 @@ def cull_SnapsByDate(snapTaskDetail):
             if lastCull != cullDateStr:
                 lastCull = cullDateStr
             else:
-                subprocess.check_output(['zfs', 'destroy', snapName])
+                subprocess.call(['zfs', 'destroy', snapName])
 
 def main():
 
-    # Call FreeNAS API to get the system's periodic snapshot tasks. Then create
-    # a list with the task's filesystem and snapshot combined retention count
-    # and unit. Example: "2" and "week" becomes "2w"
-    snapTaskDetail = []
-    for a in get_snapshottasks():
-        b = [a['task_filesystem'], str(a['task_ret_count']) + a['task_ret_unit'][0:1]]
-        # Tasks with duplicate filesystem and retention critera are combined.
-        if b not in snapTaskDetail:
-            snapTaskDetail.append(b)
-
+    # By default the configured 'Periodic Snapshot Tasks' are used as a source
+    # for what snapshots to cull. The script gets these from the FreeNAS API. If
+    # you want a more targeted or customized approach you can instead create
+    # your own list of what tasks to cull. Pass your list to the cull functions.
+    # Example list: [['pool/dataset', '2w'], etc.]
+    snapTaskDetail = getTaskDetail()
+    
+    # Comment out either cull method below as desired.
     cull_IdenticalSnaps(snapTaskDetail)
     cull_SnapsByDate(snapTaskDetail)
 
